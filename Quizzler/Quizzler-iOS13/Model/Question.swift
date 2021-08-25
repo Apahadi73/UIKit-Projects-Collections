b918a11b1050747a383fbde0d37cd59261b6129e
Amir Pahadi <apahadi73@gmail.com>
2021-08-25 18:15:49 -0500

    Quizzler added

---
+++ Quizzler/Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Amir Pahadi on 11/01/2020.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+}
+
+    
